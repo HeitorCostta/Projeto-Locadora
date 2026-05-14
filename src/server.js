809fe317f713485229cf4e0a95af356f817de1f6
Heitorcostta <heitor.fdcosta@gmail.com>
2026-05-14 15:42:50 -0300

    feat: deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const app = require('./app');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
